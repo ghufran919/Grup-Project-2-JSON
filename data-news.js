bb4883f262cc0b77c3b37b62f5114f5589acac4c
ghufran919 <ahmadghufranakbar@gmail.com>
2022-09-12 06:57:34 +0800

    init json news

---
+++ data-news.js
@@ -0,0 +1,93 @@
+const data = [
+  {
+    id: 1,
+    title: "Begini Cara Membersihkan Sisa Susu di Lidah Bayi, Moms!",
+    type: "article",
+    desc: "Ingin bersihkan sisa susu putih-putih di lidah bayi? Begini caranya!",
+    URL: "https://www.fimela.com/parenting/read/3807333/begini-cara-membersihkan-sisa-susu-di-lidah-bayi-moms",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/PyxP9610ys17PDqZ8WhFbOe7VCU=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2524860/original/093064400_1544622191-begini-cara-membersihkan-sisa-susu-di-lidah-bayi-moms.jpg",
+  },
+  {
+    id: 2,
+    title: "Cara Menebalkan Alis Bayi dengan Cara Alami, Manfaatkan Kemiri",
+    type: "article",
+    desc: "Yuk, coba cara alami ini.",
+    URL: "https://www.fimela.com/parenting/read/3778342/cara-menebalkan-alis-bayi-dengan-cara-alami-manfaatkan-kemiri",
+    picture:
+      "https://cdn1-production-images-kly.akamaized.net/pYeW_xQNAnhOLSrMsn-tRJdKqZM=/50x0:717x667/200x200/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3557199/original/029569300_1630462794-shutterstock_491857810.jpg",
+  },
+  {
+    id: 3,
+    title: "10 Makanan Sehat untuk Si Kecil yang Sedang Sakit",
+    type: "article",
+    desc: "Beberapa jenis asupan yang diperlukan untuk si kecil saat mengalami sakit.",
+    URL: "https://www.fimela.com/parenting/read/3725986/10-makanan-sehat-untuk-si-kecil-yang-sedang-sakit",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/pb36KMGBOIxl3mUGkrG7PAGgcus=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3959426/original/043611900_1646972006-shutterstock_1922789642.jpg",
+  },
+  {
+    id: 4,
+    title: "Ini 5 Manfaat Zat Besi untuk Bayi dan Anak, Bisa Mengurangi Risiko Kematian",
+    type: "article",
+    desc: "Intip 5 manfaat zat besi untuk bayi dan anak.",
+    URL: "https://www.fimela.com/parenting/read/4720467/ini-5-manfaat-zat-besi-untuk-bayi-dan-anak-bisa-mengurangi-risiko-kematian",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/T6ThVkWRqSs4c13sOydG3m5_oSE=/1360x766/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/3643930/original/095481800_1637821734-pexels-alex-green-5692269.jpg",
+  },
+  {
+    id: 5,
+    title: "Usia 1-2 Bulan, Apa yang Sudah Bisa Dilakukan Bayi?",
+    type: "article",
+    desc: "Apa saja yang sudah bisa dilakukan bayi usia 1 bulan? Catat perkembangannya berikut ini.",
+    URL: "https://www.fimela.com/parenting/read/3876042/usia-1-2-bulan-apa-yang-sudah-bisa-dilakukan-bayi",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/PR--w35efSRr36G804AKzWcnvAM=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2709044/original/012770900_1548051340-adorable-baby-bed-1455403.jpg",
+  },
+  {
+    id: 6,
+    title: "Bubur Bayi Instan Apakah Aman untuk Menu MPASI Bayi?",
+    type: "article",
+    desc: "Seberapa aman bubur instan buat bayi?",
+    URL: "https://www.fimela.com/parenting/read/3866753/bubur-bayi-instan-apakah-aman-untuk-menu-mpasi-bayi",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/VrKQKgvpD1ZmV27a7M05AaJHdEg=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2397518/original/020268700_1541047226-kazuend-127416-unsplash.JPG",
+  },
+  {
+    id: 7,
+    title: "Mengenal Perbedaan Gumoh dan Muntah pada Bayi",
+    type: "article",
+    desc: "Gumoh dan muntah merupakan kondisi di mana bayi mengeluarkan cairan putih dari mulutnya.",
+    URL: "https://www.fimela.com/parenting/read/3862945/mengenal-perbedaan-gumoh-dan-muntah-pada-bayi",
+    picture: "https://cdn1-production-images-kly.akamaized.net/0QLcNmIf5M-6WD0OlU9pRbv9lgA=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2112966/original/088303800_1524471048-iStock-503229252.jpg",
+  },
+  {
+    id: 8,
+    title: "Kapan Sebaiknya Anak Diperkenalkan dengan Telur Ayam?",
+    type: "article",
+    desc: "Saat anak masih berusia 0-6 bulan sebaiknya ia diberi ASI eksklusif. Setelah 6 bulan, ia boleh diberi MPASI.",
+    URL: "https://www.fimela.com/parenting/read/3812153/kapan-sebaiknya-anak-diperkenalkan-dengan-telur-ayam",
+    picture:
+      "https://cdn1-production-images-kly.akamaized.net/S5YjBwemRd-vxlKDmV5zyNTPFTk=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2532335/original/036985900_1544718505-8-nama-bayi-laki-laki-unik-awalan-huruf-a-yang-punya-arti-baik.jpg",
+  },
+  {
+    id: 9,
+    title: "Cara Memilih Biskuit Enak dan Sehat Buat MPASI Bayi",
+    type: "article",
+    desc: "Sejak zaman dulu biskuit dikenal sebagai nutrisi terbaik untuk tumbuh kembang bayi.",
+    URL: "https://www.fimela.com/parenting/read/3812151/cara-memilih-biskuit-enak-dan-sehat-buat-mpasi-bayi",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/AQVagLUd8smMjraPcOU09eyxXYk=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2532232/original/009984700_1544716428-cara-memilih-biskuit-enak-dan-sehat-buat-mpasi-bayi.jpg",
+  },
+  {
+    id: 10,
+    title: "Bayi Terlilit Tali Pusar Saat di Kandungan, Apakah Berbahaya?",
+    desc: "Bayi yang lehernya terlilit tali pusarnya sendiri adalah hal yang sering terjadi.",
+    type: "article",
+    URL: "https://www.fimela.com/parenting/read/3812118/bayi-terlilit-tali-pusar-saat-di-kandungan-apakah-berbahaya",
+    picture:
+      "https://cdn0-production-images-kly.akamaized.net/OnHwVxZr5XV3ykDTycFPGPVZExY=/200x200/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/2532183/original/050227500_1544715405-bayi-terlilit-tali-pusar-saat-di-kandungan-apakah-berbahaya.jpg",
+  },
+];
+
+export default data;
